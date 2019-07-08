7dd8e6e2348c2c5d2cd20d8c2780b5ff8d97376f
gvannest <gautier.vanneste42@gmail.com>
2019-07-08 18:18:07 +0200

    done! A faire bonuses + tester comme un singouin

--- equation.py
+++ equation.py
@@ -1,6 +1,7 @@
 import sys
 
 from collections import deque
+from math import sqrt
 
 from utils import dic_precedence, ft_error
 from X_class import X
@@ -23,9 +24,13 @@ class Equation:
 		self.eq_init = ''
 		self.equation_str = line
 		self.tree = []
-		self.reduced_tree = deque()
+		self.reduced_elem = {}
 		self.reduced_str = ''
 		self.flag_h = flag
+		self.degree = None
+		self.delta = None
+		self.solution1 = None
+		self.solution2 = None
 
 	def parsing_errors(self):
 		if '=' not in self.equation_str:
@@ -128,55 +133,88 @@ class Equation:
 					output_stack.append(e.value)
 			# ft_print_tree(e.value)
 			complete_newtree(new_tree, e.value)
-			return sorted(new_tree)
+			return new_tree
 
-		def reduce_final(new_tree):
-			self.reduced_tree = []
-			tmp_list = []
-			if len(new_tree) == 1:
-				self.reduced_tree.append(new_tree[0])
-				return None
+		def complete_dict(new_tree):
 			for e in new_tree:
-				if len(tmp_list) < 1:
-					tmp_list.append(e)
-				elif len(tmp_list) == 1:
-					tmp_list.append(e)
-					o = Operator('+')
-					o.right = tmp_list.pop()
-					o.left = tmp_list.pop()
-					o.evaluate()
-					if isinstance(o.value, X):
-						tmp_list.append(o.value)
-					else:
-						self.reduced_tree.append(o.left)
-						tmp_list.append(o.right)
-			while tmp_list:
-				self.reduced_tree.append(tmp_list.pop())
+				if e.power not in self.reduced_elem.keys():
+					self.reduced_elem[e.power] = e
+				else:
+					self.reduced_elem[e.power] = self.reduced_elem[e.power] + e
 			return None
 
 		def reduced_equation():
-			for i,e in enumerate(self.reduced_tree):
-				if self.flag_h and not e.factor:
+			for i,e in enumerate(sorted(list(self.reduced_elem.keys()))):
+				if self.flag_h and not self.reduced_elem[e].factor:
 					continue
-				if not e.factor % 1:
-					e.factor = int(e.factor)
-				if not e.power % 1:
-					e.power = int(e.power)
+				if not self.reduced_elem[e].factor % 1:
+					self.reduced_elem[e].factor = int(self.reduced_elem[e].factor)
+				if not self.reduced_elem[e].power % 1:
+					self.reduced_elem[e].power = int(self.reduced_elem[e].power)
 				if i == 0:
-					self.reduced_str += e.__str__()
-				else :
-					self.reduced_str += ' + ' + e.__str__()
+					self.reduced_str += self.reduced_elem[e].__str__()
+				else:
+					self.reduced_str += ' + ' + self.reduced_elem[e].__str__()
 			self.reduced_str += ' = 0'
 			self.reduced_str = self.reduced_str.replace('+ -', '- ')
 			if self.flag_h:
 				self.reduced_str = self.reduced_str.replace(' * X^0', '').replace('X^1', 'X').replace('1 * ', '')
 			return None
 
-		new_tree = reduce_intermediate()
-		reduce_final(new_tree)
-		reduced_equation()
+		def ft_getdegree():
+			for k,v in self.reduced_elem.items():
+				if self.degree is None or (k > self.degree and v.factor != 0):
+					self.degree = k
+			if not self.degree % 1:
+				self.degree = int(self.degree)
+			return None
 
+		new_tree = reduce_intermediate()
+		complete_dict(new_tree)
+		reduced_equation()
+		ft_getdegree()
 		return None
+
+	def solve_equation(self):
+		a = 0
+		b = 0
+		c = 0
+		for k,v in self.reduced_elem.items():
+			if k > 2:
+				ft_error("This equation is of a degree higher than 2. This algorithm is not built to solve it.")
+			if k == 2:
+				a = v.factor
+			elif k == 1:
+				b = v.factor
+			elif k == 0:
+				c = v.factor
+		if not a and not b and not c:
+			print("Always true. Infinity of possible values for X.")
+			sys.exit(0)
+		elif not a and not b and c:
+			ft_error("Impossible equation.")
+		elif (not a and b and not c) or (a and not b and not c):
+			self.solution1 = 0
+		elif not a and b and c:
+			self.solution1 = -c / b
+		else:
+			self.delta = b ** 2 - 4 * a * c
+			if self.delta == 0:
+				self.solution1 = -b / (2 * a)
+			elif self.delta > 0:
+				self.solution1 = (-b - sqrt(self.delta)) / (2 * a)
+				self.solution2 = (-b + sqrt(self.delta)) / (2 * a)
+			elif self.delta < 0:
+				self.solution1 = f"({-b} - i * {sqrt(-self.delta)}) / {2 * a}"
+				self.solution2 = f"({-b} + i * {sqrt(-self.delta)}) / {2 * a}"
+		if not self.solution1 % 1:
+			self.solution1 = int(self.solution1)
+		if self.solution2 and not self.solution2 % 1:
+			self.solution2 = int(self.solution2)
+		return None
+
+
+
 
 
 
